5d3d120596ac917defbe86983548aa03e6bdb171
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-06 08:53:36 +0800

    Add files via upload

---
+++ leetcode_no112_路经总和_w1.cpp
@@ -0,0 +1,62 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*  递归法
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了95.73%的用户
+*	内存消耗：17.7 MB, 在所有 C++ 提交中击败了20.47%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool hasPathSum(TreeNode* root, int sum) {
+    if (!root) {
+        return false;
+    }
+    if (root->val == sum && !root->left && !root->right) {
+        return true;
+    }
+    return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+}
